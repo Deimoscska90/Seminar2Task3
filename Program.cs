8807bc477ae79d42cc8daec8ab862737631bc68b
Filipp <Filipp_nikolaev@rambler.ru>
2023-01-31 23:22:43 +0600

    Task3

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int numberRequest(string message)
+ {
+Console.WriteLine(message);
+int number = Convert.ToInt32(Console.ReadLine());
+return number; 
+ }
+
+int Enternumber = numberRequest("Введите число:");
+
+if (Enternumber == 6 || Enternumber == 7)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
+
+
